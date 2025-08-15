bb6688e0a0e00f7a52c69ea6a692f75c4ef47e3a
Alanna Tomaz <160501486+alannatomaz@users.noreply.github.com>
2025-08-15 17:07:01 -0300

    Create Ex1_OlaSeuNomeE.java

---
+++ Ex1_OlaSeuNomeE.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex1_OlaSeuNomeE {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome: ");
+		String nome = scanner.nextLine();
+		
+		System.out.println("Digite sua idade: ");
+		int idade = scanner.nextInt();
+		
+		System.out.println("Olá, " + nome + " Alanvocê tem " + idade + " anos");
+		
+		scanner.close();
+		
+	}
+
+}

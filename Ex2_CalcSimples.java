eb7a5477fcfab99f7950d58054975ec1e5a2f4c5
Alanna Tomaz <160501486+alannatomaz@users.noreply.github.com>
2025-08-28 14:43:27 -0300

    Update Ex2_CalcSimples.java

--- Ex2_CalcSimples.java
+++ Ex2_CalcSimples.java
@@ -1,27 +1,39 @@
-import java.util.Arrays;
+/* Exercicio 2: Escreva um programa que pegue dois numeros inteiros como entrada e 
+ * permita ao usuario escolher uma das quatro operações basicas(soma, subtração, multiplicação ou divisão.
+ * Realize a operação escolhida e imprima o resultado*/
+
 import java.util.Scanner;
 
-public class Algoritmo3 {
-
+public class Ex2_CalcSimples {
 	public static void main(String[] args) {
-
-		System.out.println("Insira uma string: ");
 		Scanner scanner = new Scanner(System.in);
-		String texto = scanner.nextLine();
+		
+		System.out.println("Insira um numero: ");
+		int numero1 = scanner.nextInt();
 
-		char[] textoArray = texto.toCharArray();
-		System.out.println(Arrays.toString(textoArray));
-
-		for (int i = 0, j = textoArray.length - 1; i <= j; i++, j--) {
-			char inicio = textoArray[i];
-			char fim = textoArray[j];
-			char aux = inicio;
-			textoArray[i] = fim;
-			textoArray[j] = aux;
-		}
-
-		System.out.println(String.valueOf(textoArray));
+		System.out.println("Insira um numero: ");
+		int numero2 = scanner.nextInt();
+		
+		System.out.println("Escolha entre as operações: \n1- Soma \n2- Subtração \n3- Multiplicacao \n4- Divisao");
+		
+		int operacao = scanner.nextInt();
+		
+		switch (operacao) {
+        case 1:
+            System.out.println("Resultado: " + (numero1 + numero2));
+            break;
+        case 2:
+            System.out.println("Resultado: " + (numero1 - numero2));
+            break;
+        case 3:
+            System.out.println("Resultado: " + (numero1 * numero2));
+            break;
+        case 4: 
+        	System.out.println("Resultado: " + ((double) numero1 / numero2));
+        	break;
+        }
+		
+		scanner.close();
 
 	}
-
-}
+}	

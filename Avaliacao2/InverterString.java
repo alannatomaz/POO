5cb13d2e84d0a09b8312ced5c667b60ceacf9737
Alanna Tomaz <160501486+alannatomaz@users.noreply.github.com>
2025-08-28 18:16:56 -0300

    Create InverterString.java

---
+++ Avaliacao2/InverterString.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InverterString {
+  
+public static void main(String[] args) {
+  
+  System.out.println("Insira uma string: ");
+  Scanner scanner = new Scanner(System.in);
+  
+  String texto = scanner.nextLine();
+  
+  char[] textoArray = texto.toCharArray();
+    System.out.println(Arrays.toString(textoArray));
+  
+    for (int i = 0, j = textoArray.length - 1; i <= j; i++, j--) {
+    char inicio = textoArray[i];
+    char fim = textoArray[j];
+    char aux = inicio;
+      
+    textoArray[i] = fim;
+    textoArray[j] = aux;
+    }
+  System.out.println(String.valueOf(textoArray));
+}
